f20b075bbb5ee9f55abf4d57b8ca84f8563673f8
Rob de Kort <info@studio1902.nl>
2020-04-15 11:03:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,9 +23,6 @@ fi
 # Update homebrew recipes
 brew update
 
-# Install NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-
 # Install Homebrew Packages
 cd ~
 echo "Installing Homebrew packages"
