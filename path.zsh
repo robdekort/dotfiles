bbcdf44afd46bb247128e73a7bcc8875232f54db
Rob de Kort <rob@studio1902.nl>
2020-04-17 11:58:28 +0200

    Add msyql@5.7 to path

--- path.zsh
+++ path.zsh
@@ -1,5 +1,5 @@
 export PATH="$PATH:$HOME/.composer/vendor/bin"
-export PATH=$PATH:/usr/local/mysql/
+export PATH=$PATH:/usr/local/opt/mysql@5.7/bin/
 export PATH=$PATH:/usr/local/sbin/
 export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
 export PATH=/usr/local/bin:$PATH
