dbf38a7a2548cf6db4f3f56817d5a694b4660ba3
Rob de Kort <rob@studio1902.nl>
2020-04-17 10:34:21 +0200

    Update path

--- path.zsh
+++ path.zsh
@@ -1,5 +1,6 @@
 export PATH="$PATH:$HOME/.composer/vendor/bin"
-export PATH=$PATH:/usr/local/mysql/bin/
+export PATH=$PATH:/usr/local/mysql/
+export PATH=$PATH:/usr/local/sbin/
 export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
 export PATH=/usr/local/bin:$PATH
 
