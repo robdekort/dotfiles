1faeddb74bb353fbb9276b7081a81f77bbbfdb02
Rob de Kort <rob@studio1902.nl>
2020-04-17 09:34:42 +0200

    Don’t make hidden apps translucent

--- macos.sh
+++ macos.sh
@@ -329,7 +329,7 @@ defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide -bool true
 
 # Make Dock icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool true
+# defaults write com.apple.dock showhidden -bool false
 
 # Don’t show recent applications in Dock
 # defaults write com.apple.dock show-recents -bool false
